9585892e4c309889cfe3565149d8a9aa580fa09f
Akrelia <romain.richard.chaudron@gmail.com>
2023-02-19 02:27:07 +0100

    Cursor

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,52 @@ const canvas = document.getElementById("game");
 const context = canvas.getContext("2d");
 const tileset = new Image();
 const cursor = new Image();
+let mapData = null;
+let tileSize = 0;
+let mousePosition = new Vector(0, 0); 
 tileset.src = "assets/tileset.png";
 cursor.src = "assets/cursor.png";
 canvas.addEventListener('mousemove', mouseMove);
 
-loadMap();
+initialize();
 
-async function loadMap()
+function gameLoop() 
 {
+    update();
+    draw();
+    window.requestAnimationFrame(gameLoop);
+}
 
-    var mapData = await getJSON("data/map.json");
-    var tileSize = mapData.tileSize;
+function update()
+{
+}
 
+function draw()
+{
+    context.clearRect(0, 0, canvas.width, canvas.height); // Clean tout
+    
+    drawMap(); // Draw the map
+
+    drawCursor(); // Draww the cursor
+}
+
+async function initialize()
+{
+    mapData = await getJSON("data/map.json");
+    tileSize = mapData.tileSize;
     canvas.height = mapData.data.length * tileSize;
     canvas.width = mapData.data[0].length  * tileSize;
 
+    window.requestAnimationFrame(gameLoop);
+}
+
+function drawCursor()
+{
+    context.drawImage(cursor, Math.floor(mousePosition.x / tileSize) * tileSize, Math.floor(mousePosition.y / tileSize) * tileSize);
+}
+
+function drawMap()
+{
     for (let y = 0; y < mapData.data[0].length; y++)
     {
         for (let x = 0; x < mapData.data.length; x++)
@@ -28,7 +59,5 @@ async function loadMap()
 
 function mouseMove(e)
 {
-    let mousePos = new Vector(e.offsetX, e.offsetY);
-
-    context.drawImage(cursor, mousePos.x, mousePos.y);
+    mousePosition = new Vector(e.offsetX, e.offsetY);
 }
