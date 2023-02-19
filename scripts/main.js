db114116cffc411da822d38cdf2739a3641d5468
Akrelia <romain.richard.chaudron@gmail.com>
2023-02-19 01:57:10 +0100

    Clean

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,11 @@ const canvas = document.getElementById("game");
 const context = canvas.getContext("2d");
 const tileset = new Image();
 tileset.src = "assets/tileset.png";
-    
+
 loadMap();
 
 async function loadMap()
 {
-
-
     var mapData = await getJSON("data/map.json");
     var tileSize = mapData.tileSize;
 
