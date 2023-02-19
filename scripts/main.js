9d89d40b81190dca2abb8932f14c630ba2be8acc
Akrelia <romain.richard.chaudron@gmail.com>
2023-02-19 01:37:06 +0100

    Test Map

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,26 @@
 const canvas = document.getElementById("game");
 const context = canvas.getContext("2d");
-const tileset = new Image();
-tileset.src = "assets/tileset.png";
 
 
 loadMap();
 
 async function loadMap()
 {
+    const tileset = new Image();
+
+    tileset.src = "assets/tileset.png";
+
     var mapData = await getJSON("data/map.json");
-    context.drawImage(tileset, 0, 0);
-    console.log(mapData.name);
+
+    var tileSize = mapData.tileSize;
+
+    console.log(tileset);
+
+    for (let y = 0; y < mapData.data[0].length; y++)
+    {
+        for (let x = 0; x < mapData.data.length; x++)
+        {
+            context.drawImage(tileset, tileSize * mapData.data[x][y], 0, tileSize, tileSize, x * tileSize, y * tileSize, tileSize, tileSize);
+        }
+    }
 }
