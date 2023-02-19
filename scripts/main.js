54bf010331b8345ec27545a3ab0330f2d0be6c95
Akrelia <romain.richard.chaudron@gmail.com>
2023-02-19 01:14:07 +0100

    Map

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,14 @@
 const canvas = document.getElementById("game");
 const context = canvas.getContext("2d");
+const tileset = new Image();
+tileset.src = "assets/tileset.png";
+
+
+loadMap();
+
+async function loadMap()
+{
+    var mapData = await getJSON("data/map.json");
+    context.drawImage(tileset, 0, 0);
+    console.log(mapData.name);
+}
