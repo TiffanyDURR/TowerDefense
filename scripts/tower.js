bcd0b5b75e02cf2dd9438a3ffcec72d9dfdeb9a6
TIFF DURR <TFFNDRR@GMAIL.COM>
2023-02-19 14:37:11 +0100

    script tower

--- scripts/tower.js
+++ scripts/tower.js
@@ -1,1 +1,13 @@
-class Tower {}
+class Tower {
+  towerImage = new Image();
+
+  async load() {
+    this.towerImage.src = "assets/tower.png";
+  }
+
+  update() {}
+
+  draw(context, x, y) {
+    context.drawImage(this.towerImage, x, y);
+  }
+}
